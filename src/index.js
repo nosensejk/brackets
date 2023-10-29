f4c71638926125776da3f451477833281b54ba28
Anton Tur <antoxatur@gmail.com>
2023-10-29 14:55:39 +0300

    fix: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const dlina = str.length / 2;
+  const brackets = bracketsConfig.flat().join().replaceAll(",", "").match(/.{1,2}/g);
+
+  for (let i = 0; i < dlina; i++) {
+    for (let bracket of brackets) {
+      if (str.includes(bracket)) {
+        str = str.replace(bracket, "");
+      }
+    }
+  }
+  if (str) return false;
+  else return true;
+  
 }
